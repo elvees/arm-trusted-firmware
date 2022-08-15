a2771999f39755f60d3193583cd65f9bd8a2984f
Alexander Ugorelov <a.ugorelov@trustlab.center>
2022-08-15 17:48:12 +0300

    mcom03: Fix formatting according to project requirements
    Issue: #MCOM03SW-1483
    Change-Id: Id2d98f0ca161508592ad2808bc076f8c43b69621

--- plat/elvees/mcom03/include/platform_def.h
+++ plat/elvees/mcom03/include/platform_def.h
@@ -12,51 +12,85 @@
 #include <plat/common/common_def.h>
 
 /* CPU topology */
-#define PLAT_MAX_CORES_PER_CLUSTER	4
-#define PLAT_CLUSTER_COUNT		1
-#define PLATFORM_CORE_COUNT		(PLAT_CLUSTER_COUNT *	\
+#define PLAT_MAX_CORES_PER_CLUSTER	U(4)
+#define PLAT_CLUSTER_COUNT		U(1)
+#define PLATFORM_CORE_COUNT		(PLAT_CLUSTER_COUNT * \
 					 PLAT_MAX_CORES_PER_CLUSTER)
 
-#define PLAT_MAX_PWR_LVL		MPIDR_AFFLVL1
-#define PLAT_MAX_RET_STATE		MPIDR_AFFLVL1
-#define PLAT_MAX_OFF_STATE		MPIDR_AFFLVL2
+#define PLAT_MAX_PWR_LVL		(MPIDR_AFFLVL1)
 
-#define PLAT_LOCAL_STATE_RUN		0
-#define PLAT_LOCAL_STATE_RET		1
-#define PLAT_LOCAL_STATE_OFF		2
+/*
+ * This macro defines the deepest retention state possible. A higher state
+ * id will represent an invalid or a power down state.
+ */
+#define PLAT_MAX_RET_STATE		U(1)
 
-#define CACHE_WRITEBACK_SHIFT		6
-#define CACHE_WRITEBACK_GRANULE		(1 << CACHE_WRITEBACK_SHIFT)
+/*
+ * This macro defines the deepest power down states possible. Any state ID
+ * higher than this is invalid.
+ */
+#define PLAT_MAX_OFF_STATE		U(2)
 
-#define PLAT_PHY_ADDR_SPACE_SIZE	(1ULL << 40)
-#define PLAT_VIRT_ADDR_SPACE_SIZE	(1ULL << 39)
-#define MAX_XLAT_TABLES			8
+/* Local power state for power domains in Run state. */
+#define PLAT_LOCAL_STATE_RUN		U(0)
+/* Local power state for retention. Valid only for CPU power domains */
+#define PLAT_LOCAL_STATE_RET		U(1)
+/* Local power state for power-down. Valid for CPU and cluster power domains. */
+#define PLAT_LOCAL_STATE_OFF		U(2)
+
+/* Size of cacheable stacks */
+#define PLATFORM_STACK_SIZE		UL(0x400)
+
+/*
+ * Some data must be aligned on the biggest cache line size in the platform.
+ * This is known only to the platform as it might have a combination of
+ * integrated and external caches.
+ */
+#define CACHE_WRITEBACK_SHIFT		U(6)
+#define CACHE_WRITEBACK_GRANULE		(U(1) << CACHE_WRITEBACK_SHIFT)
+
+/* Memory-related defines */
+#define PLAT_PHY_ADDR_SPACE_SIZE	(ULL(1) << 40)
+#define PLAT_VIRT_ADDR_SPACE_SIZE	(ULL(1) << 39)
+
 #define MAX_MMAP_REGIONS		8
+#define MAX_XLAT_TABLES			8
 
-#define PLATFORM_STACK_SIZE		0x400
-
+/*
+ * BL3-1 specific defines.
+ */
 #define BL31_BASE			0x80000000
 #define BL31_SIZE			0x00080000
 #define BL31_LIMIT			(BL31_BASE + BL31_SIZE)
 
+/*
+ * BL3-3 specific defines.
+ */
 #define BL33_BASE			BL31_LIMIT
 
 /* TIMER related constants */
 #ifdef PLAT_MCOM03_HAPS
 #define PLAT_TIMER_CLK			5000000
-#define PLAT_BOOT_UART_BASE		0x1640000
-#define PLAT_BOOT_UART_CLK_IN_HZ	14750000
 #else
 #define PLAT_TIMER_CLK			27000000
-#define PLAT_BOOT_UART_BASE		0x1750000
-#define PLAT_BOOT_UART_CLK_IN_HZ	27000000
 #endif
 
+/* UART related constants */
+#ifdef PLAT_MCOM03_HAPS
+#define PLAT_BOOT_UART_BASE		0x1640000
+#define PLAT_BOOT_UART_CLK_IN_HZ	14750000
 #define PLAT_BOOT_CONSOLE_BAUDRATE	115200
+#else
+#define PLAT_BOOT_UART_BASE		0x1750000
+#define PLAT_BOOT_UART_CLK_IN_HZ	27000000
+#define PLAT_BOOT_CONSOLE_BAUDRATE	115200
+#endif
 
+/* Base MCom-03 platform compatible GIC memory map */
 #define PLAT_GICD_BASE			0x1100000
 #define PLAT_GICR_BASE			0x1180000
 
+/* SGI, PPI */
 #define PLAT_IRQ_SEC_PHY_TIMER		29
 
 #define PLAT_IRQ_SEC_SGI_0		8
--- plat/elvees/mcom03/xlat_setup.c
+++ plat/elvees/mcom03/xlat_setup.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 RnD Center "ELVEES", JSC
+ *Copyright 2019-2022 RnD Center "ELVEES", JSC
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -9,7 +9,7 @@
 #include <common/debug.h>
 #include <lib/xlat_tables/xlat_tables_v2.h>
 
-#define PLAT_REG_REGION_BASE	0x0000000ULL
+#define PLAT_REG_REGION_BASE	0x00000000ULL
 #define PLAT_REG_REGION_SIZE	0x60000000ULL
 
 #define PLAT_GICD_REGION_BASE	PLAT_GICD_BASE
@@ -18,8 +18,8 @@
 #define PLAT_GICR_REGION_BASE	PLAT_GICR_BASE
 #define PLAT_GICR_REGION_SIZE	0x80000
 
-#define URB_REGION_BASE	PLAT_URB_BASE
-#define URB_REGION_SIZE	0x10000
+#define URB_REGION_BASE		PLAT_URB_BASE
+#define URB_REGION_SIZE		0x10000
 
 void mcom03_mmap_setup(uintptr_t total_base, size_t total_size,
 		       const struct mmap_region *mmap)
